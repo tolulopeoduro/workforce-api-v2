c41f109ba9b3c71dfa73403bc4e6b546e06ca2b8
tolulopeoduro <tolulopeoduro345@gmail.com>
2021-11-22 07:41:16 +0000

    added updatePost endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { createPost } from "../controllers/posts/createPost";
 import { getAllPosts } from "../controllers/posts/getAllPosts";
 import { getOnePost } from "../controllers/posts/getOnePost";
+import { updatePost } from "../controllers/posts/updatePost";
 
 const postRoutes = new Router()
 
 postRoutes.post("/" , createPost)
 postRoutes.get("/" , getAllPosts)
-postRoutes.get("/:id" , getOnePost )
+postRoutes.get("/:id" , getOnePost)
+postRoutes.put("/:id" , updatePost)
 
 export default postRoutes
