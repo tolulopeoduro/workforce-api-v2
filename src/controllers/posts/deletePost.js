a6c2897460b42c6589231f9348452604d66c5523
tolulopeoduro <tolulopeoduro345@gmail.com>
2021-11-22 20:51:47 +0000

    added deletePost endpoint

---
+++ src/controllers/posts/deletePost.js
@@ -0,0 +1,15 @@
+const { ObjectId } = require("bson");
+
+exports.deletePost = async (req , res) => {
+    console.log(req.params)
+    const client = req.app.locals.db;
+    const post = await client.db("workforce-v2").collection("posts").find({_id : ObjectId(req.params.id)}).toArray()
+    console.log(post)
+    client.db("workforce-v2").collection("posts").deleteOne({_id : ObjectId(req.params.id)})
+    .then(() => {
+        res.status(200).json({
+          message : 'down'  
+        })
+
+    })
+}
