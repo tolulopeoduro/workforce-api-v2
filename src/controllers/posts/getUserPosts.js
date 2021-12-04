2f55e7ddb17c9e30fc9f415f5738b2fda21cfae0
tolulopeoduro <tolulopeoduro345@gmail.com>
2021-12-04 09:03:54 +0000

    fixes

---
+++ src/controllers/posts/getUserPosts.js
@@ -0,0 +1,7 @@
+exports.getUserPosts = async (req , res) => {
+    const client = req.app.locals.db;
+    const post = await client.db("workforce-v2").collection("posts").find({author : req.params.id}).toArray()
+    res.status(200).json({
+        data : post
+    })
+}
