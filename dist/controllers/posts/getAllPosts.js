63fe3c6cbeff7541ccd70f44ae2060265f27e47c
tolulopeoduro <tolulopeoduro345@gmail.com>
2021-12-05 09:15:05 +0000

    fixes

--- dist/controllers/posts/getAllPosts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.getAllPosts = async (req, res) => {
-  const client = req.app.locals.db;
-  const posts = await client.db("workforce-v2").collection("posts").find().toArray();
-  res.status(200).json({
-    data: posts
-  });
-};
